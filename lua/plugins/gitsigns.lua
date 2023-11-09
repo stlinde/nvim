432d353a89fd1c910a6b8b07e049a174faf116dc
Sebastian Tved Linde <stl@yco.dk>
2023-11-09 11:19:34 +0100

    refactor(gitsigns): small refactor

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,7 +2,8 @@ return {
 	"lewis6991/gitsigns.nvim",
 	lazy = false,
 	config = function()
-		require("gitsigns").setup({
+		local gitsigns = require("gitsigns")
+		gitsigns.setup({
 			signs = {
 				add = { text = "│" },
 				change = { text = "│" },
