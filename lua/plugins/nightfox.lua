6b9d2ed55be316ab33a96632c860cabe3a8513e0
Sebastian Hempel Linde <slinde@debian.hempellinde>
2023-11-09 06:44:25 +0100

    changed colorscheme to material ocean

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 999,
-    config = function()
-      vim.cmd("colorscheme nightfox")
-    end
-  }
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 999,
+		config = function()
+			-- vim.cmd("colorscheme nightfox")
+		end,
+	},
 }
